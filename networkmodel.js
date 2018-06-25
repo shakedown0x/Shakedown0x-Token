dda092f26d2853b30c2132c58f84bab9affe3bed
Shakedown0x <40376657+shakedown0x@users.noreply.github.com>
2018-06-25 12:00:10 -0800

    Create networkmodel.js

---
+++ networkmodel.js
@@ -0,0 +1,12 @@
+
+'use strict';
+
+module.exports = NetworkModel;
+
+function NetworkModel(networkId, provider, from, gasLimit, gasPrice) {
+    this.network_id = networkId;
+    this.provider = provider;
+    this.from = from;
+    this.gas = gasLimit || 4700000;
+    this.gasPrice = gasPrice || 21 * (10 ** 9);
+}
